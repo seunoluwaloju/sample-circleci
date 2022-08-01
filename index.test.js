564171a9e8952daf66812f2982389e955c803f17
Seun Akintimehin <seun.akintimehin@coherehealth>
2022-08-01 10:03:23 -0500

    update config file to build and test application

--- index.test.js
+++ index.test.js
@@ -11,10 +11,6 @@ let container
 
 describe('index.html', () => {
     beforeEach(() => {
-        // Constructing a new JSDOM with this option is the key
-        // to getting the code in the script tag to execute.
-        // This is indeed dangerous and should only be done with trusted content.
-        // https://github.com/jsdom/jsdom#executing-scripts
         dom = new JSDOM(html, { runScripts: 'dangerously' })
         container = dom.window.document.body
     })
